b2f31899029408bcb29d8bbb93f9cb16078ddeea
Deepa Peddamail <dpm676@ads.northwestern.edu>
2018-05-08 20:05:24 -0500

    'may8th'

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 
   
     $('#addrinput').addClass('animated fadeInUp');
+    $('#AddrSubmit').addClass('animated infinite pulse');
 
    
     $(".navbar a").click(function(){
@@ -13,23 +14,46 @@ $(document).ready(function(){
     })
    
     $('#AddrSubmit').on('click', function(){
-        console.log($(".navbar").height());
-        $("body,html").animate({
-        scrollTop:$("#" + $(this).data('value')).offset().top
-        },1000)
-        
-        $("#" + $(this).data('value')).height($(window).height());
-        console.log($('#startaddr').val());
-    })
 
+        window.location.href = 'MapsWeather.html';
+    return false;
+        
+        // $("body,html").animate({
+        // scrollTop:$("#" + $(this).data('value')).offset().top
+        // },1000)
+        
+        // $("#" + $(this).data('value')).height($(window).height());
+        // console.log($('#startaddr').val());
+    })
+/* autopopulate the address logic*/
+    $("#startaddr").geocomplete({
+        details: ".geo-details",
+        detailsAttribute: "data-geo"
+    }).bind("geocode:result", function(event, result) {
+        console.log(event);
+        console.log(result);
+    });
+/* End of autopopulate the address logic*/
+
+/* autopopulate the End address logic*/
+$("#endaddr").geocomplete({
+    details: ".geo-details",
+    detailsAttribute: "data-geo"
+}).bind("geocode:result", function(event, result) {
+    console.log(event);
+    console.log(result);
+});
+/* End of autopopulate the End address logic*/
+   
+    
     $("#addrinput").on( "keydown", function( event ) {  
         $(event.delegateTarget ).css( "background-color", "transparent");
         $("#startaddr").css({"background-color": "blue", "color":"white"} );
         $("#endaddr").css({"background-color": "blue", "color":"white"} );
+        $("#AddrSubmit").css({"background-color": "blue", "color":"white", "border-color":"white", "border-width":"4px"} );
         // $('document.body').css("background-image","url('../images/bike2.jpg')");
         // $('document.body').css("background-color","white");
-      
- 
+       
       });
     
 })
